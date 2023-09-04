df4a33422c509d9d539ad76640dcbe2dde20e006
0xAurelou <0xaurelou@gmail.com>
2023-09-04 11:40:05 +0900

    wip: communication done need to fix address problem
    Signed-off-by: 0xAurelou <0xaurelou@gmail.com>

---
+++ src/lzERC20Eth.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {OFTV2} from "lz/token/oft/v2/OFTV2.sol";
+
+import "forge-std/console.sol";
+
+/**
+ * @author  0xAurelou
+ * @title   ERC20 LayerZero
+ * @dev     Do not use in production this is for research purposes only
+ * @notice  ERC20 using LayerZero
+ */
+
+contract LzERC20Eth is OFTV2 {
+    constructor(
+        address _layerZeroEndpoint,
+        uint256 _initialSupply,
+        uint8 _sharedDecimals
+    ) OFTV2("LzERC20Eth", "lzETHEth", _sharedDecimals, _layerZeroEndpoint) {
+        _mint(_msgSender(), _initialSupply);
+    }
+}
---
+++ test/lzERC20.t.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "src/lzERC20Eth.sol";
+import "src/lzERC20Arb.sol";
+import "lz/mocks/LZEndpointMock.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+import "lz/token/oft/v2/ICommonOFT.sol";
+
+contract lzERC20Test is Test {
+    uint16 chainIdEth = 1;
+    uint16 chainIdArb = 42161;
+    LZEndpointMock lzEndpointEth;
+    LZEndpointMock lzEndpointArb;
+    LzERC20Eth lzERC20Eth;
+    LzERC20Arb lzERC20Arb;
+    address aliceEth = address(0x10);
+    address bobArb = address(0x11);
+
+    function setUp() public {
+        vm.startPrank(aliceEth);
+
+
+        // Creation of Ethereum LzEndpointMock
+        lzEndpointEth = new LZEndpointMock(chainIdEth);
+        console.log("lzEndpointEth", address(lzEndpointEth));
+
+        // Creation of Arbitrum LzEndpointMock
+        lzEndpointArb = new LZEndpointMock(chainIdArb);
+        console.log("lzEndpointArb", address(lzEndpointArb));
+
+        // Creation of Ethereum Lz token
+        lzERC20Eth = new LzERC20Eth(address(lzEndpointEth), 1000e18, 18);
+
+        // Creation of Arbitrum Lz token
+        lzERC20Arb = new LzERC20Arb(address(lzEndpointArb), 0, 18);
+
+        // Set the LzEndpointMock destination chain
+        lzEndpointEth.setDestLzEndpoint(
+            address(lzERC20Arb),
+            address(lzEndpointArb)
+        );
+
+        // Set the LzEndpointMock destination chain
+        lzEndpointArb.setDestLzEndpoint(
+            address(lzERC20Eth),
+            address(lzEndpointEth)
+        );
+    
+        // We give alice 1ether for the gas fees
+        vm.deal(aliceEth, 1 ether);
+
+        vm.prank(aliceEth);
+
+        // We set the trustedRemote
+        lzERC20Eth.setTrustedRemote(
+            chainIdArb,
+            abi.encodePacked(lzERC20Arb, lzERC20Eth)
+        );
+
+
+        vm.prank(aliceEth);
+        
+        // We set the trustedRemote
+        lzERC20Arb.setTrustedRemote(
+            chainIdEth,
+            abi.encodePacked(lzERC20Eth, lzERC20Arb)
+        );
+
+        
+    }
+
+    function testSetup() public {
+        assertEq(lzEndpointEth.getChainId(), chainIdEth);
+        assertEq(lzEndpointArb.getChainId(), chainIdArb);
+        assertEq(lzERC20Eth.totalSupply(), 1000e18);
+        assertEq(lzERC20Eth.balanceOf(aliceEth), 1000e18);
+        assertEq(lzERC20Arb.balanceOf(bobArb), 0);
+    }
+
+    function testTransferEthereumToArbitrum() public {
+        uint fees = 0;
+        
+        // We estimate fees
+        (fees, ) = lzERC20Eth.estimateSendFee(
+            chainIdArb,
+            bytes32(uint(uint160(bobArb))),
+            10e18,
+            false,
+            ""
+        );
+
+        // Check if the fees computation have been done correctly 
+        assertTrue(fees != 0);
+
+        vm.prank(aliceEth);
+        lzERC20Eth.approve(address(lzEndpointEth), 1e18);
+
+        // callData paramater
+        ICommonOFT.LzCallParams memory callParams = ICommonOFT.LzCallParams(
+            payable(aliceEth),
+            address(0),
+            ""
+        );
+
+        // We encode the bobArb address in bytes32
+        bytes memory bobBytes = abi.encodePacked(bobArb);
+
+        vm.prank(aliceEth);
+        // We send 10 lzERC20Eth to the Arbitrum chain
+        lzERC20Eth.sendFrom{value: 1 ether}(
+            aliceEth,
+            chainIdArb,
+            bytes32(bobBytes),
+            10e18,
+            callParams
+        );
+
+        assertEq(lzERC20Eth.balanceOf(aliceEth), 0);
+        assertEq(lzERC20Arb.balanceOf(bobArb), 10e18);
+    }
+}
